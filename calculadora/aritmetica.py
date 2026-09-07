d85d419b1cc03c0c207c80f0e9630556dc889d70
horwincb-arch <horwincb@uamv.edu.ni>
2026-09-07 13:40:12 -0600

    agrego el archivo calculadora

---
+++ calculadora/aritmetica.py
@@ -0,0 +1,15 @@
+"""""Operaciones
+suma, resta, multiplicacion y division"""
+def add(number1, number2):
+    return number1 + number2
+def sub(number1, number2):
+    return number1 - number2
+def mult(number1, number2):
+    return number1 * number2
+def div(number1, number2):
+    try:
+        return number1 / number2
+    except ZeroDivisionError:
+        return "No se puede dividir entre 0."
+    except ValueError:
+        return "Tipo de dato incorrecto, debe ingresar un numero."
---
+++ calculadora/principal.py
@@ -0,0 +1,3 @@
+import aritmetica as arit
+print(f"La suma es: {arit.add(2,2)}")
+print(f"La resta es: {arit.sub(5,3)}")
